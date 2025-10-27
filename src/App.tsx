810844477e3f664adbd43320d5f0103f55a31c9e
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-10-27 10:11:56 +0600

    Basic Counter with Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './redux/features/counterSlice';
+
+
 
 
 function App() {
 
+  const dispatch = useDispatch()
+  const { count } = useSelector((state) => state.counter)
 
   return (
-    <>
-      <div>
-        Home
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex border border-purple-300 rounded-md p-10 bg-slate-200">
+        <button onClick={() => dispatch(increment())} className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">Increment</button>
+        <h1 className="text-3xl mx-10">{count}</h1>
+        <button onClick={() => dispatch(decrement())} className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold text-white">Decrement</button>
       </div>
-    </>
+    </div>
   )
 }
 
