09b8d699b98e01a03fcc9da7ee10a1b0393ea649
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-10-28 09:54:04 +0600

    Devtools, middlwares and custome middlwares

---
+++ src/redux/middleware/logger.tsx
@@ -0,0 +1,10 @@
+
+
+const Logger = (state) => (next) => (action) => {
+    console.log("current State=>", state.getState());
+    console.log("Action=>", action);
+    next(action);
+    console.log("current State=>", state.getState());
+}
+
+export default Logger;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counterSlice";
+import Logger from "./middleware/logger";
 
 
-
-export const store = configureStore(
-    {
-        reducer: {
-            counter: counterReducer
-        }
-    }
-)
-
-
-
+export const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(Logger)
+});
 
 export type RootState = ReturnType<typeof store.getState>
-
 export type AppDispatch = typeof store.dispatch
